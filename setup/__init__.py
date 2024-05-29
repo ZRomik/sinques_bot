d2b69c794c20e013eb084400e2db50f65de1db74
Raziel Sotken <zromik@mail.ru>
2024-05-29 14:30:54 +0400

    Обновен код инициализации бота.

---
+++ setup/__init__.py
@@ -0,0 +1,2 @@
+from .log_setup import *
+from .bot_setup import bot, dp
---
+++ setup/bot_setup.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher
+from helpers import read_param
+from exceptions import *
+import logging
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+logger = logging.getLogger(__name__)
+
+token = read_param("TOKEN")
+if not token:
+    logger.critical("Не указан токен бота. Работа невозможна.")
+    raise ImproperlyConfigured("Не указан токен бота. Работа невозможна.")
+
+mems = MemoryStorage()
+
+bot = Bot(
+    token=token
+)
+
+dp = Dispatcher(
+    bot=bot,
+    storage=mems
+)
