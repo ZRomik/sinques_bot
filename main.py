f6f69f98b51d604916af0c530ea10df4b843f255
Raziel Sotken <zromik@mail.ru>
2024-05-29 14:28:12 +0400

    Инициализация системы логирования перенесена из главного скрипта в пакет setup.

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 import logging
-
-LOG_FORMAT = "%(asctime)s [%(levelname)s]: %(message)s"
-DATE_FORMAT = "%d.%m.%Y, %H:%M:%S"
+from aiogram.utils import executor
+from setup import *
+from helpers import read_param
+from os import path
 
 logger = logging.getLogger(__name__)
 
+async def startup(_):
+    logger.info("Бот запущен")
+
+async def shutdown(_):
+    logger.info("Завершение работы бота.")
+
 def main():
-    logger.info("Setting up bot")
+    logger.info("Подготовка к запуску бота.")
     # отключение логгеров библиотек, чтобы не засорять консоль
     logging.getLogger("requests").setLevel(logging.ERROR)
     logging.getLogger("aiogram").setLevel(logging.ERROR)
     logging.getLogger("urllib3").setLevel(logging.ERROR)
-    logger.info("Done. Running bot.")
+    if dp:
+        logger.info("Запуск бота...")
+        executor.start_polling(
+            dispatcher=dp,
+            on_startup=startup,
+            on_shutdown=shutdown,
+            timeout=3,
+            skip_updates=True
+        )
 
 if __name__ == "__main__":
-    # настройка логгера
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format= LOG_FORMAT,
-        datefmt= DATE_FORMAT
-    )
-    log_name = "work.log"
-    fh = logging.FileHandler(log_name)
-    fmt = logging.Formatter(LOG_FORMAT)
-    fmt.datefmt = DATE_FORMAT
-    fh.setLevel(logging.WARNING)
-    logging.getLogger().addHandler(fh)
-    logging.basicConfig(
-        handlers=[
-            fh
-        ]
-    )
-
     main()
---
+++ setup/log_setup.py
@@ -0,0 +1,28 @@
+import logging
+from helpers import read_param
+
+LOG_FORMAT = "%(asctime)s [%(levelname)s]: %(message)s"
+DATE_FORMAT = "%d.%m.%Y, %H:%M:%S"
+
+# настройка системы логирования
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=LOG_FORMAT,
+    datefmt=DATE_FORMAT
+)
+log_name = read_param("LOGNAME")
+if not log_name:
+    log_name = "work.log"
+fh = logging.FileHandler(log_name)
+fh.encoding = "utf-8"
+fh.mode = "w"
+fmt = logging.Formatter(LOG_FORMAT)
+fmt.datefmt = DATE_FORMAT
+fh.setFormatter(fmt)
+fh.setLevel(logging.WARNING)
+logging.getLogger().addHandler(fh)
+logging.basicConfig(
+    handlers=[
+        fh,
+    ]
+)
