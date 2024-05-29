6cc57265a801af60a60eab1788fa27958f381707
Raziel Sotken <zromik@mail.ru>
2024-05-29 11:33:45 +0400

    Реализована функция чтения параметров конфигурации.

---
+++ helpers/config_reader.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def read_param(key: str):
+    """
+    Функция для чтения параметров конфигурации.
+    Считывает значение указанного параметра файла конфигурации и возвращает его.
+    Если параметра ен существует, возвращает None
+    :param key: (str) - имя параметра
+    :return: str | None
+    """
+    return os.getenv(key)
