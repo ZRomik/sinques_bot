3ff2b044962416c8f713e2c9e402429565fd7368
Raziel Sotken <zromik@mail.ru>
2024-07-01 13:56:18 +0400

    Реализовано тестирование общей модели.

---
+++ tests/test_db_tables_creation.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+from peewee import *
+from models import *
+from db_connection import database
+
+class DatabaseTestCase(TestCase):
+    """
+    Тестирование БД
+    """
+    def setUp(self) -> None:
+        super().setUp()
+        self.tables = [
+            BaseModel
+        ]
+
+    def tearDown(self) -> None:
+        database.drop_tables(self.tables)
+
+    def test_create_tables(self):
+        for table in self.tables:
+            database.create_tables([table])
